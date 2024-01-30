4c188ebadfb21a194bdb2ea6ccc6b704dfc3bb21
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2024-01-30 16:38:47 -0300

    refact: service beneficiaries and excel

--- .history/src/excel/excel.service_20240129175245.ts
+++ .history/src/excel/excel.service_20240129175245.ts
@@ -1,7 +1,7 @@
+// excel.service.ts
 import { Injectable } from '@nestjs/common';
 import * as ExcelJS from 'exceljs';
 import { CustomWebSocketGateway } from '../notification/index';
-import * as fs from 'fs';
 
 @Injectable()
 export class ExcelService {
@@ -11,10 +11,10 @@ export class ExcelService {
     const workbook = new ExcelJS.Workbook();
     const worksheet = workbook.addWorksheet('Beneficiarios');
 
-    // Add headers
+    // Adiciona cabeçalhos
     worksheet.addRow(['ID', 'Nome', 'Email', 'Telefone']);
 
-    // Add beneficiary data
+    // Adiciona os dados dos beneficiários
     beneficiarios.forEach((beneficiario) => {
       worksheet.addRow([
         beneficiario.id,
@@ -24,32 +24,13 @@ export class ExcelService {
       ]);
     });
 
+    // Salva o arquivo
     const fileName = 'beneficiarios.xlsx';
-    const outputStream = fs.createWriteStream(fileName);
+    await workbook.xlsx.writeFile(fileName);
 
-    // Write Excel file to the stream
-    await workbook.xlsx.write(outputStream);
-
-    // Close the Excel file output stream
-    outputStream.end();
-
-    const memoryUsage = process.memoryUsage();
-    console.log(`Memory used: ${formatBytes(memoryUsage.heapUsed)}`);
-
-    // Notify the user when the spreadsheet is ready
+    // Notifica o usuário quando a planilha estiver pronta
     this.webSocketGateway.notifyUser(
       `Planilha de beneficiários gerada com sucesso! Faça o download em: ${fileName}`,
     );
   }
-}
-
-function formatBytes(bytes: number): string {
-  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
-  if (bytes === 0) {
-    return '0 Byte';
-  }
-
-  const i = Math.floor(Math.log(bytes) / Math.log(1024));
-  return parseFloat((bytes / Math.pow(1024, i)).toFixed(2)) + ' ' + sizes[i];
 }
